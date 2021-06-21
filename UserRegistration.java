2c1d616e13ef42b6dd820545899984e2c0ba1c2c
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-21 22:40:42 +0530

    refactor: Test cases to validate user details

--- UserRegistration.java
+++ UserRegistration.java
@@ -9,6 +9,7 @@
 
 package regexpattern;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Scanner;
 
@@ -16,109 +17,56 @@ public class UserRegistration {
 
 	static User user = new User();
 	static Scanner sc = new Scanner(System.in);
-	String regPattern = "^[A-Z]{1}[a-z]{2,}$";
+	
 
 	/**
-	 * UC-1 Method to validate first name
+	 * UC-1 UC-2 Method to validate first name and last name
 	 */
-	public static boolean getFirstName() { 
-		System.out.println("Enter first name");
-		String firstNmae = sc.next();
-		boolean check = Pattern.matches("^[A-Z]{1}[a-z]{2,}$", firstNmae);
-		if (check) {
-			user.setFirstName(firstNmae);
-		} else {
-			System.out.println("Invalid first name");
-			getFirstName();
-		}
-		return false;
-	}
+	public boolean getFirstName(String name) {
 
-	/**
-	 * UC-2 Method to validate Last name
-	 */
-	public static boolean getLastName() { 
-		
-		System.out.println("Enter Last name");
-		String lastName = sc.next();
-		boolean check = Pattern.matches("^[A-Z]{1}[a-z]{2,}$", lastName);
-		if (check) {
-			user.setLastName(lastName);
-		} else {
-			System.out.println("Invalid last name");
-			getLastName();
-		}
-		return false;
+		Pattern pattern = Pattern.compile(("^[A-Z]{1}[a-z]{2,}"));
+		Matcher matcher = pattern.matcher(name);
+		return matcher.matches();
 	}
 
 	/**
 	 * UC-3 Method to validate Email ID
 	 */
-	public static boolean getEmail() {
-		
-		System.out.println("Enter Email ID");
-		String emailID = sc.next();
-		boolean check = Pattern.matches(
-				"^([a-z]{2,}[0-9a-z]{1,}([_+-.*$#]{0,1}[a-z0-9]{1,}){0,1}[@]{1}[a-z0-1]{1,}[.]{1}[a-z]{2,4}([.]{0,1}[a-z]{2}){0,1})$",
-				emailID);
-		if (check) {
-			user.setEmail(emailID);
-		} else {
-			System.out.println("Invalid email");
-			getEmail();
-		}
-		return false;
+	public boolean getEmail(String email) {
+
+		Pattern pattern = Pattern.compile(
+				("^([a-z]{2,}[0-9a-z]{1,}([_+-.*$#]{0,1}[a-z0-9]{1,}){0,1}[@]{1}[a-z0-1]{1,}[.]{1}[a-z]{2,4}([.]{0,1}[a-z]{2}){0,1})$"));
+		Matcher matcher = pattern.matcher(email);
+		boolean matches = matcher.matches();
+
+		return matches;
 	}
 
 	/**
 	 * UC-4 Method to validate Mobile number with country code
 	 */
-	public static boolean getMobileNumber() {
-		
-		System.out.println("Enter Mobile Number");
-		String phoneNo = sc.next();
-		boolean check = Pattern.matches("^(\\+91)[7-9][0-9]{9}$", phoneNo);
-		if (check) {
-			user.setMobileNumber(phoneNo);
-		} else {
-			System.out.println("Invalid Mobile No.");
-			getMobileNumber();
-		}
-		return false;
+	public boolean getMobileNumber(String phoneNumber) {
+
+		Pattern pattern = Pattern.compile(("^[\\+?][9?][1?][\\s\\-?][\\d]{10}"));
+		Matcher matcher = pattern.matcher(phoneNumber);
+		boolean matches = matcher.matches();
+
+		return matches;
 	}
 
 	/**
-	 * UC-5 Password Rule No.1- It should contain at least 8 characters 
-	 * UC-6 Password Rule No.2- It should contain at least one Upper case 
-	 * UC-7 Password Rule No.3- It should contain at least one number 
-	 * UC-8 Password Rule No.4- It should contain exact one special character
+	 * UC-5 Password Rule No.1- It should contain at least 8 characters UC-6
+	 * Password Rule No.2- It should contain at least one Upper case UC-7 Password
+	 * Rule No.3- It should contain at least one number UC-8 Password Rule No.4- It
+	 * should contain exact one special character
 	 * 
 	 */
-	public static boolean getPassword() {
-		
-		System.out.println("Enter Password");
-		String password = sc.next();
-		boolean check = Pattern.matches(
-				"^((?=[^@|#|&|%|*]*[@|&|#|%|*][^@|#|&|%|*]*$)(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9@#$%&*]{8,})$", password);
-		if (check) {
-			user.setMobileNumber(password);
-		} else {
-			System.out.println("Password should contain at least 8 character");
-			System.out.println("Password should contain at least one Upper case");
-			System.out.println("Password should contain at least one number");
-			System.out.println("Password should contain exact one special character");
-			getPassword();
-		}
-		return false;
-	}
+	public boolean getPassword(String password) {
 
-	public static void main(String[] args) {
-
-		getFirstName();
-		getLastName();	
-		getEmail();
-		getMobileNumber();
-		getPassword();
+		Pattern pattern = Pattern.compile("^(?=.*[@#$%^&+=])(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$");
+		Matcher matcher = pattern.matcher(password);
+		boolean matches = matcher.matches();
+		return matches;
 	}
 
 }
--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -1,246 +1,221 @@
 package regexpattern;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+
 
 public class UserRegistrationTest {
 
-	UserRegistration testObject = new UserRegistration();
+	
+	  UserRegistration userRegistration = new UserRegistration();
 
 	/**
 	 * Test cases for first name
 	 */
 	@Test
-	public void givenValidFirstName_shouldReturnTrue() {
-		boolean firstName = UserRegistration.getFirstName();
-		Assert.assertTrue("Sanobar", firstName);
-	}
+    public void givenValidFirstName_shouldReturnTrue() {
 
-	@Test
-	public void givenFirstName_whenMinimumThreeCharacter_shouldReturnTrue() {
-		boolean firstName =UserRegistration.getFirstName();
-		Assert.assertTrue("San", firstName);
-	}
+        boolean name = userRegistration.getFirstName("Sanobar");
+        assertTrue(name);
 
-	@Test
-	public void givenInvalidFirstName_shouldReturnFalse() {
-		boolean firstName = UserRegistration.getFirstName();
-		Assert.assertFalse("Sa", firstName);
-	}
+    }
 
-	@Test
-	public void givenInvalidFirstName_WithSmallChar_shouldReturnfalse() {
+    @Test
+    public void givenFirstName_whenMinimumThreeCharacter_shouldReturnTrue() {
+        boolean name = userRegistration.getFirstName("San");
+        assertTrue(name);
+    }
 
-		boolean firstName = UserRegistration.getFirstName();
-		Assert.assertFalse("sanobar", firstName);
-	}
-	
-	@Test
-	public void givenInvalidFirstName_WithonlyOneChar_shouldReturnfalse() {
+    @Test
+    public void givenFirstName_LessThanThreecharacter_shouldReturnFalse() {
+        boolean name = userRegistration.getFirstName("Sa");
+        assertFalse(name);
+    }
 
-		boolean firstName = UserRegistration.getFirstName();
-		Assert.assertFalse("S", firstName);
-	}
+    @Test
+    public void givenInvalidFirstName_WithSmallChar_shouldReturnfalse() {
 
-	@Test
-	public void givenFirstName_whenAllUpperCaseCharacter_shouldReturnFalse() {
-		boolean firstName = UserRegistration.getFirstName();
-		Assert.assertFalse("SANOBAR", firstName);
-	}
+        boolean name = userRegistration.getFirstName("sanobar");
+        assertFalse(name);
+    }
 
-	@Test
-	public void givenFirstName_whenContainNumber_shouldReturnFalse() {
-		boolean firstName = UserRegistration.getFirstName();
-		Assert.assertFalse("San478", firstName);
-	}
 
-	
-	/**
-	 * Test cases for Last name
-	 */
-	@Test
-	public void givenValidLastName_shouldReturnTrue() {
-		boolean lastname = UserRegistration.getLastName();
-		Assert.assertTrue("Mujawar", lastname);
-	}
+    @Test
+    public void givenFirstName_whenAllUpperCaseCharacter_shouldReturnFalse() {
+        boolean name = userRegistration.getFirstName("SANOBAR");
+        assertFalse(name);
+    }
 
-	@Test
-	public void givenLastName_whenMinimumThreeCharacter_shouldReturnTrue() {
-		boolean lastname =UserRegistration.getLastName();
-		Assert.assertTrue("Muj",lastname);
-	}
+    @Test
+    public void givenFirstName_whenContainNumber_shouldReturnFalse() {
+        boolean name = userRegistration.getFirstName("Sa784nk");
+        assertFalse(name);
+    }
+    
+     @Test
+    public void givenFirstName_whenContainSpecialChars_shouldReturnFalse() {
+        boolean name = userRegistration.getFirstName("Sano@bar");
+        assertFalse(name);
+    }
+    
 
-	@Test
-	public void givenInvalidLastName_shouldReturnFalse() {
-		boolean lastname = UserRegistration.getLastName();
-		Assert.assertFalse("Mu", lastname);
-	}
+    /**
+     * Test cases for email ID
+     *
+     */
+    
+    @Test
+    public void givenEmailId_whenProper_shouldReturnTrue() {
+        boolean emailId = userRegistration.getEmail("sanobar.mujawar@gmail.com");
+        assertTrue(emailId);
+    }
 
-	@Test
-	public void givenInvalidLastName_WithSmallChar_shouldReturnfalse() {
+    @Test
+    public void givenEmailId_whenContainSymbol_shouldReturnTrue() {
+        boolean emailId = userRegistration.getEmail("sanobar.123@gmail.com");
+        assertTrue(emailId);
+    }
 
-		boolean lastname = UserRegistration.getLastName();
-		Assert.assertFalse("mujawar", lastname);
-	}
-	
-	@Test
-	public void givenInvalidLastName_WithonlyOneChar_shouldReturnfalse() {
+    @Test
+    public void givenEmailId_whenContainLessThanThreeCharacter_shouldReturnFalse() {
+        boolean emailId = userRegistration.getEmail("sa@gmail.com");
+        assertFalse(emailId);
+    }
 
-		boolean lastname = UserRegistration.getLastName();
-		Assert.assertFalse("M", lastname);
-	}
+    @Test
+    public void givenEmailId_whenNotContainGmailPart_shouldReturnFalse() {
+        boolean emailId = userRegistration.getEmail("sanobar@.com");
+        assertFalse(emailId);
+    }
 
-	@Test
-	public void givenLastName_whenAllUpperCaseCharacter_shouldReturnFalse() {
-		boolean lastname = UserRegistration.getLastName();
-		Assert.assertFalse("MUJAWAR", lastname);
-	}
+    @Test
+    public void givenEmailId_whenNotContainComPart_shouldReturnFalse() {
+        boolean emailId = userRegistration.getEmail("sanobar@gmail");
+        assertFalse(emailId);
+    }
 
-	@Test
-	public void givenLastName_whenContainNumber_shouldReturnFalse() {
-		boolean lastname = UserRegistration.getLastName();
-		Assert.assertFalse("Muja478", lastname);
-	}
+    @Test
+    public void givenEmailID_havingContinuousDots_shouldReturnFalse() {
+        boolean emailId = userRegistration.getEmail("abc..28@gmail.com");
+        assertFalse(emailId);
+    }
 
-	/**
-	 * Test cases for email ID
-	 * 
-	 */
-	 @Test
-	    public void givenEmailId_whenProper_shouldReturnTrue() {
-	        boolean emailId =UserRegistration.getEmail();
-	        Assert.assertTrue("sanobarmujawar28@gmail.com", emailId);
-	    }
+    @Test
+    public void givenEmailID_lasTLDmustcontainAtLeast2char_shouldReturnFalse() {
+        boolean emailId = userRegistration.getEmail("abc123@gmail.c");
+       assertFalse(emailId);
+    }
 
-	    @Test
-	    public void givenEmailId_whenContainSymbol_shouldReturnTrue() {
-	        boolean emailId = UserRegistration.getEmail();
-	        Assert.assertTrue("sanobar.mujawar28@gmail.com", emailId);
-	    }
+    @Test
+    public void givenEmailID_DoubleAtSymbol_shouldReturnFalse() {
+        boolean emailId = userRegistration.getEmail("abc@abc@gmail.com");
+        assertFalse(emailId);
+    }
+    
+     @Test
+    public void givenEmailID_DotAtStart_shouldReturnFalse() {
+        boolean emailId = userRegistration.getEmail(".abc@abc@gmail.com");
+        assertFalse(emailId);
+    }
+    
+    @Test
+    public void givenEmailID_MultipleTopLevelDomain_AtEnd_shouldReturnFalse() {
+        boolean emailId = userRegistration.getEmail("abc@gmail.com.aa.au");
+        assertFalse(emailId);
+    }
 
-	    @Test
-	    public void givenEmailId_whenContainLessThanThreeCharacter_shouldReturnFalse() {
-	        boolean emailId = UserRegistration.getEmail();
-	        Assert.assertFalse("sa@gmail.com", emailId);
-	    }
 
-	    @Test
-	    public void givenEmailId_whenNotContainGmailPart_shouldReturnFalse() {
-	        boolean emailId = UserRegistration.getEmail();
-	        Assert.assertFalse("sana@.com", emailId);
-	    }
+    /**
+     * Test cases for Mobile Numbers.
+     */
+    
+    
+    @Test
+    public void givenMobileNumber_whenProper_shouldReturnTrue() {
+        boolean mobileNumber = userRegistration. getMobileNumber("+91-7676878789");
+        assertTrue(mobileNumber);
+    }
 
-	    @Test
-	    public void givenEmailId_whenNotContainComPart_shouldReturnFalse() {
-	        boolean emailId = UserRegistration.getEmail();
-	        Assert.assertFalse("sana@gmail", emailId);
-	    }
-	    
-	    @Test
-	    public void givenEmailID_havingContinuousDots_shouldReturnFalse() {
-	    	boolean emailId = UserRegistration.getEmail();
-	        Assert.assertFalse("sana..28@gmail.com", emailId);
-	    }
-	
-	    @Test
-	    public void givenEmailID_lasTLDmustcontainAtLeast2char_shouldReturnFalse() {
-	    	boolean emailId = UserRegistration.getEmail();
-	        Assert.assertFalse("abc123@gmail.a", emailId);
-	    }
-	    
-	    @Test
-	    public void givenEmailID_DoubleAtSymbol_shouldReturnFalse() {
-	    	boolean emailId = UserRegistration.getEmail();
-	        Assert.assertFalse("abc@abc@gmail.com", emailId);
-	    }
-	    
-	    /**
-	     * Test cases for mobile no.
-	     */
-	    
-	    @Test
-	    public void givenMobileNumber_whenProper_shouldReturnTrue() {
-	        boolean mobNumber = UserRegistration.getMobileNumber();
-	        Assert.assertTrue("+917676878789", mobNumber);
-	    }
+    @Test
+    public void givenMobileNumber_whenLessThanTenDigit_shouldReturnFalse() {
+        boolean mobileNumber = userRegistration.getMobileNumber("+91-767687");
+        assertFalse(mobileNumber);
+    }
 
-	    @Test
-	    public void givenMobileNumber_whenLessThanTenDigit_shouldReturnFalse() {
-	    	boolean mobNumber = UserRegistration.getMobileNumber();
-	        Assert.assertFalse("+91767687", mobNumber);
-	    }
-	    
-	    @Test
-	    public void givenMobileNumber_whenContainsSpace_shouldReturnFalse() {
-	    	boolean mobNumber = UserRegistration.getMobileNumber();
-	        Assert.assertFalse("+91 7676876754", mobNumber);
-	    }
+    @Test
+    public void givenMobileNumber_whenContainsSpace_shouldReturnTrue() {
+        boolean mobileNumber = userRegistration.getMobileNumber("+91 7676876754");
+        assertTrue(mobileNumber);
+    }
+    
+      @Test
+    public void givenMobileNumber_whenContainsOtherThen_SpaceHyphenandPlusSymbol_shouldReturnFalse() {
+        boolean mobileNumber = userRegistration.getMobileNumber("+91@7676876754");
+        assertFalse(mobileNumber);
+    }
 
-	    @Test
-	    public void givenMobileNumber_whenNoCountryCode_shouldReturnFalse() {
-	    	boolean mobNumber = UserRegistration.getMobileNumber();
-	        Assert.assertFalse("234878789", mobNumber);
-	    }
+    @Test
+    public void givenMobileNumber_whenNoCountryCode_shouldReturnFalse() {
+        boolean mobileNumber = userRegistration.getMobileNumber("234878789");
+        assertFalse(mobileNumber);
+    }
 
-	    @Test
-	    public void givenMobileNumber_whengreaterthanTen_shouldReturnFalse() {
-	    	boolean mobNumber = UserRegistration.getMobileNumber();
-	        Assert.assertFalse("577854327865", mobNumber);
-	    }
+    @Test
+    public void givenMobileNumber_whengreaterthanTen_shouldReturnFalse() {
+        boolean mobileNumber = userRegistration.getMobileNumber("+91-85432786534");
+        assertFalse(mobileNumber);
+    }
 
-	    @Test public void givenMobileNumber_whenSpecialCharacterGiven_shouldReturnFalse() {
-	    	boolean mobNumber = UserRegistration.getMobileNumber();
-	        Assert.assertFalse("+91 76768@786", mobNumber);
-	    }
-	    
-	    
-	    /**
-	     * Test cases for password 
-	     * 
-	     */
-	    
-	    @Test
-	    public void givenPassword_whenProper_shouldReturnTrue() {
-	    	boolean password = UserRegistration.getPassword();
-	        Assert.assertTrue("@Sana123", password);
-	    }
-	    @Test
-	    public void givenPassword_whenLessThanEightCharacter_shouldReturnFalse() {
-	    	boolean password = UserRegistration.getPassword();
-	        Assert.assertFalse("sana12", password);
-	    }
+    @Test
+    public void givenMobileNumber_whenSpecialCharacterGiven_shouldReturnFalse() {
+        boolean mobileNumber = userRegistration.getMobileNumber("+91 76768@786");
+        assertFalse(mobileNumber);
+    }
+    
+    @Test
+    public void givenMobileNumber_whencharactersCharacterGiven_shouldReturnFalse() {
+        boolean mobileNumber = userRegistration.getMobileNumber("+91 76768a3786");
+        assertFalse(mobileNumber);
+    }
 
-	    @Test
-	    public void givenPassword_whenAtLeastOneUpperCase_shouldReturnTrue() {
-	    	boolean password = UserRegistration.getPassword();
-	        Assert.assertTrue("sAna@123", password);
-	    }
-	    @Test
-	    public void givenPassword_whenNotContainUpperCase_shouldReturnFalse() {
-	    	boolean password = UserRegistration.getPassword();
-	        Assert.assertFalse("@sana123", password);
-	    }
 
-	    @Test
-	    public void givenPassword_whenAtLeastOneNumericNumber_shouldReturnTrue() {
-	    	boolean password = UserRegistration.getPassword();
-	        Assert.assertTrue("1@Sanobar", password);
-	    }
-	    @Test
-	    public void givenPassword_whenNotContainNumber_shouldReturnFalse() {
-	    	boolean password = UserRegistration.getPassword();
-	        Assert.assertFalse("MujawarSANOBAR", password);
-	    }
+    /**
+     * Test cases for password
+     *
+     */
+    @Test
+    public void givenPassword_whenProper_shouldReturnTrue() {
+        boolean password = userRegistration.getPassword("@Sana123");
+        assertTrue(password);
+    }
 
-	    @Test
-	    public void givenPassword_whenExactlyOneSpecialCharacter_shouldReturnTrue() {
-	    	boolean password = UserRegistration.getPassword();
-	        Assert.assertTrue("@Sana123", password);
-	    }
+    @Test
+    public void givenPassword_whenLessThanEightCharacter_shouldReturnFalse() {
+        boolean password = userRegistration.getPassword("Sana@12");
+        assertFalse(password);
+    }
 
-	    @Test
-	    public void givenPassword_whenNotContainMoreThanOneSpecialChar_shouldReturnFalse() {
-	    	boolean password = UserRegistration.getPassword();
-	        Assert.assertFalse("Sana1@#", password);
-	    }	
+
+    @Test
+    public void givenPassword_whenNotContainUpperCase_shouldReturnFalse() {
+        boolean password = userRegistration.getPassword("sana2803#");
+        assertFalse(password);
+    }
+
+   
+
+    @Test
+    public void givenPassword_whenNotContainNumber_shouldReturnFalse() {
+        boolean password = userRegistration.getPassword("sanaSH#W");
+        assertFalse(password);
+    }
+
+  
+
+    @Test
+    public void givenPassword_whenContainNoSpecialCharacter_shouldReturnFalse() {
+        boolean password = userRegistration.getPassword("Sana45eh");
+        assertFalse(password);
+    }
 }
